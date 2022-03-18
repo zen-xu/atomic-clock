50e845f7c3b46ec9e54c0d4331627894d72efbd3
ZhengYu, Xu <zen-xu@outlook.com>
2022-03-19 01:01:08 +0800

    feat: support timezone

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
-use pyo3::prelude::*;
+use std::str::FromStr;
+
+#[macro_use]
+extern crate lazy_static;
+
+use chrono::{DateTime, FixedOffset, Local, Offset, TimeZone};
+use chrono_tz::Tz;
+use pyo3::{exceptions, prelude::*};
+
+lazy_static! {
+    static ref DEFAULT_OFFSET: FixedOffset = {
+        let now = Local::now();
+        now.offset().fix()
+    };
+}
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -9,15 +22,18 @@ fn spear(_py: Python, m: &PyModule) -> PyResult<()> {
 }
 
 #[pyclass]
-#[pyo3(text_signature = "(year, month, day, hour = 0, minute = 0, second = 0, microsecond = 0)")]
+#[pyo3(
+    text_signature = "(year, month, day, hour = 0, minute = 0, second = 0, microsecond = 0, tz = None)"
+)]
 struct Spear {
-    datetime: DateTime<Utc>,
+    datetime: DateTime<FixedOffset>,
 }
 
 #[pymethods]
+#[allow(clippy::too_many_arguments)]
 impl Spear {
     #[new]
-    #[args(hour = "0", minute = "0", second = "0", microsecond = "0")]
+    #[args(hour = "0", minute = "0", second = "0", microsecond = "0", tz = "None")]
     fn new(
         year: i32,
         month: u32,
@@ -26,16 +42,22 @@ impl Spear {
         minute: u32,
         second: u32,
         microsecond: u32,
-        // tzinfo: &str,
-    ) -> Self {
-        let datetime = DateTime::from_utc(
-            NaiveDateTime::new(
-                NaiveDate::from_ymd(year, month, day),
-                NaiveTime::from_hms_micro(hour, minute, second, microsecond),
-            ),
-            Utc,
-        );
-        Self { datetime }
+        tz: Option<&str>,
+    ) -> PyResult<Self> {
+        let offset = match tz {
+            Some(tz) => {
+                let tz = Tz::from_str(tz).map_err(exceptions::PyValueError::new_err)?;
+                tz.ymd(1970, 1, 1).offset().fix()
+            }
+            None => *DEFAULT_OFFSET,
+        };
+
+        let datetime =
+            offset
+                .ymd(year, month, day)
+                .and_hms_micro(hour, minute, second, microsecond);
+
+        Ok(Self { datetime })
     }
 
     fn __repr__(&self) -> String {
