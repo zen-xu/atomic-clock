e41cbf6abaa42297503841f0d2c8f569e65c76d0
ZhengYu, Xu <zen-xu@outlook.com>
2022-03-19 00:25:21 +0800

    feat: impl Spear class

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,48 @@
+use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
 use pyo3::prelude::*;
-
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn spear(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Spear>()?;
     Ok(())
+}
+
+#[pyclass]
+#[pyo3(text_signature = "(year, month, day, hour = 0, minute = 0, second = 0, microsecond = 0)")]
+struct Spear {
+    datetime: DateTime<Utc>,
+}
+
+#[pymethods]
+impl Spear {
+    #[new]
+    #[args(hour = "0", minute = "0", second = "0", microsecond = "0")]
+    fn new(
+        year: i32,
+        month: u32,
+        day: u32,
+        hour: u32,
+        minute: u32,
+        second: u32,
+        microsecond: u32,
+        // tzinfo: &str,
+    ) -> Self {
+        let datetime = DateTime::from_utc(
+            NaiveDateTime::new(
+                NaiveDate::from_ymd(year, month, day),
+                NaiveTime::from_hms_micro(hour, minute, second, microsecond),
+            ),
+            Utc,
+        );
+        Self { datetime }
+    }
+
+    fn __repr__(&self) -> String {
+        format!("<Spear [{}]>", self.__str__())
+    }
+
+    fn __str__(&self) -> String {
+        self.datetime.to_rfc3339()
+    }
 }
