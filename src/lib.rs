595a0cf50011ce8770545e3d1595084193dc0f15
ZhengYu, Xu <zen-xu@outlook.com>
2022-03-19 23:55:22 +0800

    feat: impl `now` and `utcnow`

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,17 @@ use std::str::FromStr;
 #[macro_use]
 extern crate lazy_static;
 
-use chrono::{DateTime, FixedOffset, Local, Offset, TimeZone};
+use chrono::{DateTime, FixedOffset, Local, Offset, TimeZone, Utc};
 use chrono_tz::Tz;
-use pyo3::{exceptions, prelude::*};
+use pyo3::{exceptions, prelude::*, types::PyType};
 
 lazy_static! {
     static ref DEFAULT_OFFSET: FixedOffset = {
         let now = Local::now();
+        now.offset().fix()
+    };
+    static ref UTC_OFFSET: FixedOffset = {
+        let now = Utc::now();
         now.offset().fix()
     };
 }
@@ -44,13 +48,7 @@ impl Spear {
         microsecond: u32,
         tz: Option<&str>,
     ) -> PyResult<Self> {
-        let offset = match tz {
-            Some(tz) => {
-                let tz = Tz::from_str(tz).map_err(exceptions::PyValueError::new_err)?;
-                tz.ymd(1970, 1, 1).offset().fix()
-            }
-            None => *DEFAULT_OFFSET,
-        };
+        let offset = try_get_offset(tz)?;
 
         let datetime =
             offset
@@ -66,5 +64,34 @@ impl Spear {
 
     fn __str__(&self) -> String {
         self.datetime.to_rfc3339()
+    }
+
+    #[classmethod]
+    #[args(tz = "None")]
+    #[pyo3(text_signature = "(tz = None)")]
+    fn now(_cls: &PyType, tz: Option<&str>) -> PyResult<Self> {
+        let now = Local::now();
+        let offset = try_get_offset(tz)?;
+        let datetime = offset.from_utc_datetime(&now.naive_utc());
+        Ok(Self { datetime })
+    }
+
+    #[classmethod]
+    #[args(tz = "None")]
+    fn utcnow(_cls: &PyType) -> PyResult<Self> {
+        let now = Utc::now();
+        let offset = *UTC_OFFSET;
+        let datetime = offset.from_utc_datetime(&now.naive_utc());
+        Ok(Self { datetime })
+    }
+}
+
+fn try_get_offset(tz: Option<&str>) -> PyResult<FixedOffset> {
+    match tz {
+        Some(tz) => {
+            let tz = Tz::from_str(tz).map_err(exceptions::PyValueError::new_err)?;
+            Ok(tz.ymd(1970, 1, 1).offset().fix())
+        }
+        None => Ok(*DEFAULT_OFFSET),
     }
 }
