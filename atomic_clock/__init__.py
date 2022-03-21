024986fecd6eea5cf0d6fda19cd05f64109715fe
ZhengYu, Xu <zen-xu@outlook.com>
2022-03-21 13:08:23 +0800

    chore(stub): add python stubs

---
+++ atomic_clock/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+
+from .atomic_clock import *  # noqa: F401, F403
---
+++ atomic_clock/__init__.pyi
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+class AtomicClock:
+    """An :class:`AtomicClock <atomic_clock.AtomicClock>` object.
+
+    Implements the ``datetime`` interface, behaving as an aware ``datetime`` while implementing
+    additional functionality.
+
+    :param year: the calendar year.
+    :param month: the calendar month.
+    :param day: the calendar day.
+    :param hour: (optional) the hour. Defaults to 0.
+    :param minute: (optional) the minute, Defaults to 0.
+    :param second: (optional) the second, Defaults to 0.
+    :param microsecond: (optional) the microsecond. Defaults to 0.
+    :param tzinfo: (optional) A timezone expression.  Defaults to UTC.
+
+    .. _tz-expr:
+
+    Recognized timezone expressions:
+        - A ``str`` describing a timezone, similar to 'US/Pacific', or 'Europe/Berlin'.
+        - A ``str`` in ISO 8601 style, as in '+07:00'.
+        - A ``str``, one of the following:  'local', 'utc', 'UTC'.
+
+    Usage::
+        >>> import atomic_clock
+        >>> atomic_clock.AtomicClock(2022, 3, 20, 10, 30, 45)
+        <AtomicClock [2022-03-20T10:30:45+00:00]>
+
+    """
+
+    def __init__(
+        self,
+        year: int,
+        month: int,
+        day: int,
+        hour: int = 0,
+        minute: int = 0,
+        second: int = 0,
+        microsecond: int = 0,
+        tzinfo: str = "local",
+    ) -> None: ...
+    @classmethod
+    def now(cls, tzinfo: str = "local") -> AtomicClock:
+        """Constructs an :class:`AtomicClock <atomic_clock.AtomicClock>` object, representing "now" in the given
+        timezone.
+
+        :param tzinfo: (optional) A timezone expression. Defaults to local time.
+
+        Usage::
+            >>> AtomicClock.now('Asia/Shanghai')
+            <AtomicClock [2022-03-21T12:57:43.324231+08:00]>
+        """
+    @classmethod
+    def utcnow(cls) -> AtomicClock:
+        """Constructs an :class:`AtomicClock <atomic_clock.AtomicClock>` object, representing "now" in UTC
+        timezone.
+
+        Usage::
+            >>> AtomicClock.utcnow()
+            <AtomicClock [2022-03-21T04:58:42.796864+00:00]>
+        """
